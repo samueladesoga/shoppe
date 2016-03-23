352c44fa5c056eb283718a72fb42b906ea2783cd
Leumascom Serviced Ltd <leumascomservicedltd@Leumascoms-iMac.local>
2016-03-23 00:05:46 +0000

    removed duplicate lines

--- app/models/shoppe/customer.rb
+++ app/models/shoppe/customer.rb
@@ -11,8 +11,6 @@ module Shoppe
 
     has_many :car_details, dependent: :restrict_with_exception, class_name: "Shoppe::CarDetail"
 
-    has_many :car_details, dependent: :restrict_with_exception, class_name: "Shoppe::CarDetail"
-
     # Validations
     validates :email, presence: true, uniqueness: true, format: { with: EMAIL_REGEX}
     #validates :phone, presence: true, format: { with: PHONE_REGEX }
