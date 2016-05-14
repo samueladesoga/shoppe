3fb123275f8bb744d7a0e9a1e67fd78c02b3fefd
Leumascom Serviced Ltd <leumascomservicedltd@Leumascoms-iMac.local>
2016-05-14 13:59:01 +0100

    increased version of shoppe

--- app/models/shoppe/promo_code.rb
+++ app/models/shoppe/promo_code.rb
@@ -1,8 +1,11 @@
 module Shoppe
-
   # The Shoppe::Promo model stores promo code for discount on orders
   class PromoCode < ActiveRecord::Base
   	before_create :generate_promo_code
+
+    validates :name, presence: true
+    validates :name, uniqueness: true
+    validates :expired_date, presence: true
 
     self.table_name = 'shoppe_promo_codes'
     has_many :orders, dependent: :restrict_with_exception, class_name: 'Shoppe::Order'
