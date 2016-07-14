bd36d427ee27453bca57de36d14b3be1af52eb7b
Leumascom Serviced Ltd <leumascomservicedltd@Leumascoms-iMac.local>
2016-07-14 23:36:39 +0100

    added customer to payments page plus copied staff on order confirmation email

--- app/mailers/shoppe/order_mailer.rb
+++ app/mailers/shoppe/order_mailer.rb
@@ -2,7 +2,10 @@ module Shoppe
   class OrderMailer < ActionMailer::Base
     def received(order)
       @order = order
-      mail from: Shoppe.settings.outbound_email_address, to: order.email_address, subject: I18n.t('shoppe.order_mailer.received.subject', default: 'Order Confirmation')
+      mail from: Shoppe.settings.outbound_email_address,
+          bcc: ["#{Shoppe.settings.outbound_email_address}", "Order Watcher <#{Shoppe.settings.outbound_email_address}>"], 
+          to: order.email_address, 
+          subject: I18n.t('shoppe.order_mailer.received.subject', default: 'Order Confirmation')
     end
 
     def accepted(order)
--- lib/shoppe/version.rb
+++ lib/shoppe/version.rb
@@ -1,3 +1,3 @@
 module Shoppe
-  VERSION = "1.1.18"
+  VERSION = "1.1.21"
 end
